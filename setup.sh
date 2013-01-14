1b622c5f5c1703e786433ef1c94cb8595b476c3d
darkfe <i@darkfe.com>
2013-01-14 15:12:09 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 git submodule init
 git submodule update
 
-cp ./_vim ~/.vim
+echo 'Copy .vim folder'
+cp ./_vim ~/.vim -r 
+echo 'Copy .vimrc file'
 cp ./_vimrc ~/.vimrc
+cp 'Config pathogen plugin'
 cp ~/.vim/bundle/vim-pathogen/autoload/ ~/.vim
-
 echo 'Every thing setup~'
